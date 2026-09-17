3aadd3d1106bf647da7383eed0f4bf6611e43b4f
sonina-marina <mar1na.sonina@yandex.ru>
2026-09-17 14:19:21 +0300

    feat(optimization): implement optimize_max_sharpe function

--- optimization.py
+++ optimization.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.optimize import _constraints, minimize
+from scipy.sparse.linalg import qmr
 
 from portfolio import (
     calculate_portfolio_return,
@@ -8,7 +9,7 @@ from portfolio import (
 )
 
 
-def objective_function(weights, covariance_matrix):
+def objective_function_variance(weights, covariance_matrix):
     return calculate_portfolio_variance(weights, covariance_matrix)
 
 
@@ -23,9 +24,9 @@ def optimize_min_variance(covariance_matrix, num_assets):
     bounds = [(0,1) for _ in range(num_assets)]
 
     result = minimize(
-        objective_function,
+        objective_function_variance,
         initial_weights,
-        args=(covariance_matrix),
+        args=(covariance_matrix,),
         method="SLSQP",
         bounds=bounds,
         constraints=constraints
@@ -33,3 +34,40 @@ def optimize_min_variance(covariance_matrix, num_assets):
 
     return result
 
+
+def objective_function_sharpe(
+    weights, 
+    annual_returns,
+    covariance_matrix, 
+    risk_free_rate
+):
+
+    portfolio_return = calculate_portfolio_return(annual_returns, weights)
+    portfolio_variance = calculate_portfolio_variance(weights, covariance_matrix)
+
+    return -calculate_sharpe_ratio(portfolio_return, portfolio_variance, risk_free_rate)
+
+def optimize_max_sharpe(
+    annual_returns,
+    covariance_matrix,
+    risk_free_rate,
+    num_assets
+):
+    initial_weights = np.ones(num_assets) / num_assets
+
+    constraints = {
+        "type": "eq",
+        "fun": lambda weights: np.sum(weights) - 1
+    }
+
+    bounds = [(0, 1) for _ in range(num_assets)]
+
+    result = minimize(
+        objective_function_sharpe,
+        initial_weights,
+        args=(annual_returns, covariance_matrix, risk_free_rate),
+        bounds=bounds,
+        constraints=constraints
+    )
+
+    return result
--- portfolio.py
+++ portfolio.py
@@ -1,7 +1,6 @@
 import numpy as np
 import math
 
-
 def calculate_portfolio_return(annual_returns, weights):
     portfolio_return = (annual_returns * weights).sum()
     return portfolio_return
